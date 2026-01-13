bd36a91a856ab03b531510df03cabd793c416fac
Leandro Martello <maidenleo2000@gmail.com>
2026-01-13 13:19:04 -0300

    primer commit antes de subir

---
+++ src/FirstStepsApp.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FirstStepsApp = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Esto es un parrafo</p>
+
+      <button>Prueba de boton</button>
+
+      <div>
+        <h2>Hola dentro de un div</h2>
+      </div>
+    </>
+  );
+};
+
+export default FirstStepsApp;
---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,40 @@
+import React, { type CSSProperties } from "react";
+
+const firsName = "Leadro";
+const lastName = "Martello";
+
+const favoriteGames = ["Mafia", "Duke Nukem", "Assasins Creed"];
+const isActive = true;
+
+const myStyles: CSSProperties = {
+  backgroundColor: isActive ? "lightgreen" : "lightpink",
+  borderRadius: 10,
+  padding: 10,
+  marginTop: 30,
+};
+
+const address = {
+  zipCode: "ABC123",
+  country: "Argentina",
+};
+
+const MyAwesomeApp = () => {
+  return (
+    <>
+      <h1>{firsName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(", ")}</p>
+
+      <h1>{isActive ? "Activo" : "No activo"}</h1>
+
+      <p>{address.zipCode}</p>
+      <p>{address.country}</p>
+
+      <p>Objeto literal</p>
+      <p style={myStyles}>{JSON.stringify(address)}</p>
+    </>
+  );
+};
+
+export default MyAwesomeApp;
