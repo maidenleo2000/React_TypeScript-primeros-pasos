bce9efa897b2e29d2b739dc319f8f4dd7c4fe853
Leandro Martello <maidenleo2000@gmail.com>
2026-01-13 15:20:09 -0300

    finalizada seccion

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+// import React from "react";
 import { ItemCounter } from "./shooping-cart/ItemCounter";
+
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  { productName: "Nintendo Switch 2", quantity: 1 },
+  { productName: "Pro Controller", quantity: 2 },
+  { productName: "Obseever Pro Edition", quantity: 5 },
+];
 
 const FirstStepsApp = () => {
   return (
     <>
       <h1>Carrito de Compras</h1>
+      {/* en vez de () => {} como es la fucion flecha los parentesis al final () => () quiere decir que INMEDIATAMENTE devuelva ese objeto, es un return implícito */}
+      {itemsInCart.map(({ productName, quantity }) => (
+        <ItemCounter key={productName} name={productName} quantity={quantity} />
+      ))}
+
       {/* <p>Esto es un parrafo</p>
 
       <button>Prueba de boton</button>
@@ -12,9 +28,9 @@ const FirstStepsApp = () => {
       <div>
         <h2>Hola dentro de un div</h2>
       </div> */}
-      <ItemCounter name="Nintendo Switch 2" />
-      <ItemCounter name="Pro Controller" />
-      <ItemCounter name="Observer pro Edition" />
+      {/* <ItemCounter name="Nintendo Switch 2" quantity={1} />
+      <ItemCounter name="Pro Controller" quantity={2} />
+      <ItemCounter name="Observer pro Edition" quantity={3} /> */}
     </>
   );
 };
--- src/shooping-cart/ItemCounter.tsx
+++ src/shooping-cart/ItemCounter.tsx
@@ -1,30 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ItemCounter.css";
 
 interface Props {
   name: string;
+  //   quantity: number | undefined;
+  quantity?: number;
 }
 
-export const ItemCounter = ({ name }: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+  const [count, setCount] = useState(quantity);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubstract = () => {
+    if (count === 1) return;
+    setCount(count - 1);
+  };
+
+  //   const handleClick = () => {
+  //     console.log(`click ${name}`);
+  //   };
+
   return (
     <>
-      <section
+      {/* <section
         style={{
           display: "flex",
           alignItems: "center",
           gap: 10,
           marginTop: 10,
         }}
-      >
-        <span
+      > */}
+      <section className="item-row">
+        {/* <span
           style={{
             width: 150,
+            color: count === 1 ? "red" : "black",
           }}
+        > */}
+
+        <span
+          className="item-text"
+          style={{ color: count === 1 ? "red" : "black" }}
         >
           {name}
         </span>
-        <button>+1</button>
-        <span>10</span>
-        <button>-1</button>
+        <button
+          onMouseEnter={() => {
+            console.log(`Mouse Enter ${name}`);
+          }}
+          //   onClick={() => {
+          //     console.log(`click ${name}`);
+          //     // console.log(event);
+          //   }}
+          onClick={handleAdd}
+        >
+          +1
+        </button>
+        <span>{count}</span>
+        <button onClick={handleSubstract}>-1</button>
       </section>
     </>
   );
